35b49fdee9fe66b8d1cbfcd9c6094b5d5c9d2245
d2verb <licprog73@gmail.com>
2023-05-05 21:10:40 +0900

    post entityのリレーションを定義

--- src/app/src/Domain/Post/Entity/Post.php
+++ src/app/src/Domain/Post/Entity/Post.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace App\Domain\Post\Entity;
 
 use App\Domain\Post\Repository\PostRepositoryInterface;
+use App\Domain\User\Entity\User;
 use Cycle\Annotated\Annotation\Column;
 use Cycle\Annotated\Annotation\Entity;
+use Cycle\Annotated\Annotation\Relation;
 
 #[Entity(
     repository: PostRepositoryInterface::class,
@@ -16,9 +18,15 @@ class Post
     #[Column(type: 'primary')]
     public int $id;
 
-    #[Column(type: 'string')]
-    public string $title;
+    public function __construct(
+        #[Column(type: 'string')]
+        public string $title,
 
-    #[Column(type: 'text')]
-    public string $content;
+        #[Column(type: 'text')]
+        public string $content,
+
+        #[Relation\BelongsTo(target: User::class, nullable: false)]
+        public User $author
+    ) {
+    }
 }
