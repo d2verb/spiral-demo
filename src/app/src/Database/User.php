95921a1ca8c883df6e13d3b9b9221eb899e8650c
d2verb <licprog73@gmail.com>
2023-05-06 22:16:08 +0900

    userの永続化時にidが設定されていなかれば、自動的に設定するようにする

--- src/app/src/Database/User.php
+++ src/app/src/Database/User.php
@@ -8,11 +8,18 @@ use App\Repository\UserRepository;
 use Cycle\Annotated\Annotation\Column;
 use Cycle\Annotated\Annotation\Entity;
 use Cycle\ORM\Entity\Behavior\CreatedAt;
+use Cycle\ORM\Entity\Behavior\Event\Mapper\Command\OnCreate;
+use Cycle\ORM\Entity\Behavior\Hook;
 use Cycle\ORM\Entity\Behavior\UpdatedAt;
+use Ramsey\Uuid\Uuid;
 
 #[Entity(repository: UserRepository::class)]
 #[CreatedAt(field: 'createdAt', column: 'created_at')]
 #[UpdatedAt(field: 'updatedAt', column: 'updated_at')]
+#[Hook(
+    callable: [User::class, 'onCreate'],
+    events: OnCreate::class
+)]
 class User
 {
     #[Column(type: 'uuid', primary: true)]
@@ -25,5 +32,13 @@ class User
         #[Column(type: 'string')]
         public string $email
     ) {
+    }
+
+    public static function onCreate(OnCreate $event): void
+    {
+        $pkColumn = 'id';
+        if (!isset($event->state->getData()[$pkColumn])) {
+            $event->state->register($pkColumn, Uuid::uuid7());
+        }
     }
 }
